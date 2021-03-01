923635e47c61d22b1e45fc6cb07b0eb32ef5b7b4
horushia <michi.odk@eduge.ch>
2021-03-01 13:12:55 +0100

    Modification Script
    Vérifier les scripts avec le diagramme de classe

---
+++ 306-jeuDeLoie/Assets/Scripts/ConfigurationPartie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigurationPartie : MonoBehaviour
+{
+    private int idJoueur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void MettreAJourJoueur()
+    {
+    }
+}
