60fcf8d5ca0eb0569a4ae3b57184f37d3cd06500
Jul0thebro <63236564+Jul0thebro@users.noreply.github.com>
2021-03-15 15:54:43 +0100

    update gage par case

--- 306-jeuDeLoie/Assets/Scripts/Jeu.cs
+++ 306-jeuDeLoie/Assets/Scripts/Jeu.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class Jeu : MonoBehaviour
 {
-    public GameObject joueur;
+    public GameObject joueur1;
     public GameObject joueur2;
     public GameObject joueur3;
     public GameObject joueur4;
 
     // VARIABLES 
     public List<Joueur> joueurs = new List<Joueur>();
-    private List<Gage> gages = new List<Gage>();
     public Plateau plateau;
-    private Oie oie;    
+    private Oie oie;
+    public int joueurActuel;
 
     // Start is called before the first frame update
     void Start()
     {
-        joueurs.Add(joueur.GetComponent<Joueur>());
+        joueurs.Add(joueur1.GetComponent<Joueur>());
         joueurs.Add(joueur2.GetComponent<Joueur>());
         joueurs.Add(joueur3.GetComponent<Joueur>());
         joueurs.Add(joueur4.GetComponent<Joueur>());
+        joueurActuel = 0;
     }
 
     // Update is called once per frame
@@ -29,22 +30,25 @@ public class Jeu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            VerifierGagnant();
-            joueur.GetComponent<Transform>().SetParent(plateau.cases[4].GetComponent<Transform>());
-            joueur.GetComponent<Transform>().localPosition = new Vector3(0, 0, 0.017f);
-            joueur.GetComponent<Joueur>().emplacement = plateau.cases[4].GetComponent<Case>();
+            InitialiserJeu();
         }
+
         if (Input.GetKeyDown(KeyCode.L))
         {
-            Debug.Log("oui monsieur");
-            joueur.GetComponent<Joueur>().LancerDe();
+            joueurs[joueurActuel].LancerDe();
+            BougeJoueur(joueurs[joueurActuel], joueurs[joueurActuel].emplacement.gameObject);
+            joueurActuel++;
+            joueurActuel %= ObtientNbJoueur();
         }
     }
 
     // Initialisation du jeu 
     public void InitialiserJeu()
     {
-        
+        for (int i = 0; i < plateau.casesDepart.Count; i++)
+        {
+            BougeJoueur(joueurs[i], plateau.casesDepart[i]);
+        }
     }
 
     // Retourne le joueur qui gagne
@@ -52,7 +56,8 @@ public class Jeu : MonoBehaviour
     {
         foreach (var joueur in joueurs)
         {
-            if(joueur.emplacement == plateau.cases[plateau.cases.Count - 1]) {
+            if (joueur.emplacement == plateau.cases[plateau.cases.Count - 1])
+            {
                 return joueur;
             }
         }
@@ -69,11 +74,19 @@ public class Jeu : MonoBehaviour
         this.joueurs = joueursParDefaut;
     }
 
-    public List<Joueur> ObtientJoueurs() {
+    public List<Joueur> ObtientJoueurs()
+    {
         return joueurs;
     }
 
-    public void BougeJoueur() {
-        
+    public void BougeJoueur(Joueur joueur, GameObject c)
+    {
+        joueur.GetComponent<Transform>().SetParent(c.GetComponent<Transform>());
+        joueur.GetComponent<Transform>().localPosition = new Vector3(0, 0, 0.017f);
+        joueur.GetComponent<Joueur>().emplacement = c.GetComponent<Case>();
+        if (c.GetComponent<Case>().IdCase > 0)
+        {
+            Debug.Log(c.GetComponent<Case>().gage.description);
+        }
     }
 }
