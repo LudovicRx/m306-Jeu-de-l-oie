978d6fe1a4a97a6c8af8872a12c13a7d47a30f2d
Jul0thebro <63236564+Jul0thebro@users.noreply.github.com>
2021-03-23 09:02:22 +0100

    update color

--- 306-jeuDeLoie/Assets/Scripts/Gage.cs
+++ 306-jeuDeLoie/Assets/Scripts/Gage.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Gage : MonoBehaviour
 {
-    public static UnityEngine.Color bleu = new UnityEngine.Color(60f / 255f , 128f / 255f, 248f / 255f, 255f /255f);
-    public static UnityEngine.Color vert = new UnityEngine.Color(1f / 255f , 255f / 255f, 96f / 255f, 255f /255f);
-    public static UnityEngine.Color brun = new UnityEngine.Color(60f / 255f , 128f / 255f, 248f / 255f, 255f /255f);
-    public static UnityEngine.Color orange = new UnityEngine.Color(255f / 255f , 213f / 255f, 64f / 255f, 255f /255f);
+    public static UnityEngine.Color bleu = new UnityEngine.Color(80f / 255f , 224f / 255f, 210f / 255f, 1f);
+    public static UnityEngine.Color vert = new UnityEngine.Color(163f / 255f , 248f / 255f, 198f / 255f, 1f);
+    public static UnityEngine.Color vertFonce = new UnityEngine.Color(60f / 255f , 166f / 255f, 29f / 255f, 1f);
+    public static UnityEngine.Color jauneVert = new UnityEngine.Color(206f / 255f , 255f / 255f, 121f / 255f, 1f);
+    public static UnityEngine.Color rouge = new UnityEngine.Color(55f / 255f , 236f / 255f, 71f / 255f, 1f);
 
     public static List<Gage> gages = new List<Gage>() {
         new Gage("Fait un bras de fer avec la personne qui est à ta droite", bleu),
-        new Gage("Parle comme Yoda pendant les trois prochain tours", Color.red),
+        new Gage("Parle comme Yoda pendant les trois prochain tours", rouge),
         new Gage("Joue à ni oui ni non avec la personne à ta gauche, celui qui perd fait 10 pompes", vert),
-        new Gage("Ne parle plus pendant trois tours", Color.magenta),
-        new Gage("Fait un combat de regard avec la personne qui est à ta droite", Color.yellow)
+        new Gage("Ne parle plus pendant trois tours", vertFonce),
+        new Gage("Fait un combat de regard avec la personne qui est à ta droite", jauneVert)
         
     };
     public string description;
