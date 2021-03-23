e2dc8c82ab2ec090cdce08f50843e4c66cd4bab5
Jul0thebro <63236564+Jul0thebro@users.noreply.github.com>
2021-03-23 08:41:33 +0100

    Update Color

--- 306-jeuDeLoie/Assets/Scripts/Gage.cs
+++ 306-jeuDeLoie/Assets/Scripts/Gage.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Gage : MonoBehaviour
 {
-    public static UnityEngine.Color bleu = new UnityEngine.Color(60, 128, 248, 255);
-    
+    public static UnityEngine.Color bleu = new UnityEngine.Color(60f / 255f , 128f / 255f, 248f / 255f, 255f /255f);
+    public static UnityEngine.Color vert = new UnityEngine.Color(1f / 255f , 255f / 255f, 96f / 255f, 255f /255f);
+    public static UnityEngine.Color brun = new UnityEngine.Color(60f / 255f , 128f / 255f, 248f / 255f, 255f /255f);
+    public static UnityEngine.Color orange = new UnityEngine.Color(255f / 255f , 213f / 255f, 64f / 255f, 255f /255f);
+
     public static List<Gage> gages = new List<Gage>() {
         new Gage("Fait un bras de fer avec la personne qui est à ta droite", bleu),
         new Gage("Parle comme Yoda pendant les trois prochain tours", Color.red),
-        new Gage("Joue à ni oui ni non avec la personne à ta gauche, celui qui perd fait 10 pompes", Color.green),
+        new Gage("Joue à ni oui ni non avec la personne à ta gauche, celui qui perd fait 10 pompes", vert),
         new Gage("Ne parle plus pendant trois tours", Color.magenta),
         new Gage("Fait un combat de regard avec la personne qui est à ta droite", Color.yellow)
         
